74c9f6fe7c804d557e6110a67c9d3aa0b8617fc3
kuroidoruido <anthony.pena@outlook.fr>
2015-02-13 18:55:02 +0100

    fix in code absolute path for plugin

--- platform/src/main/java/latourextensible/platform/App.java
+++ platform/src/main/java/latourextensible/platform/App.java
@@ -6,7 +6,7 @@ public class App {
 	public static void main(String[] args) throws Exception {
 		System.out.println("Hello World!");
 		PluginManager pluginMgr = PluginManager.getDefaultInstance();
-		pluginMgr.addPath("/home/anthony/Documents/Conception de Logiciel Extensible/la-tour-extensible/platform/plugins");
+		pluginMgr.addPath((new File("./plugins")).getCanonicalPath());
 		//~ Plugin core = (Plugin)pluginMgr.getPlugin("Core");
 		//~ core.run();
 		System.out.println("getPlugin");
