9070d6c6e25b19e1c957a4e112898a4fb9dad1eb
franck2 <f.boncler@gmail.com>
2015-03-09 21:41:13 +0100

    interface creation personnage fait

--- Core/interfaces/AbstractAction.java
+++ Core/interfaces/AbstractAction.java
@@ -5,9 +5,7 @@ public abstract class AbstractAction {
 	protected String name;
 	
 	public abstract String getName();
-	
-	public abstract void setName(String n);
-	
+		
 	public abstract void doAction(AbstractCharacter personnage, AbstractMonster mob);
 
 }
